45f97a1dfd507bc2f8f6a4a81ca65189a6a0ea5f
Isaac Ross <isaac253@gmail.com>
2018-01-26 10:38:31 -0500

    evolving the aggregate_case to apply the events to aggregate, and check for errors

--- test/support/aggregate_case.ex
+++ test/support/aggregate_case.ex
@@ -7,8 +7,27 @@ defmodule Surveys.AggregateCase do
       import Surveys.Factory
 
       defp assert_event(command, expected_event) do
-        event = @aggregate_module.execute(%@aggregate_module{}, command)
+        # _agg, unused for now, is the aggregate after event has been applied to
+        # it, This will probably be useful for testing more complex aggregates
+        # in the future.
+        {_agg, event, error} = execute(command)
+
+        # assert there are no errors
+        assert is_nil(error)
+
+        # assert expected event matches up
         assert expected_event == event
+      end
+
+      defp execute(command) do
+        case @aggregate_module.execute(%@aggregate_module{}, command) do
+          {:error, reason} = error -> {%@aggregate_module{}, nil, error}
+          event -> {apply(event), event, nil}
+        end
+      end
+
+      defp apply(event) do
+        @aggregate_module.apply(%@aggregate_module{}, event)
       end
     end
   end
